c55d79c4bae864656c0680177133a15001d8d57a
bilal <bilal89@gmail.com>
2020-02-28 22:41:28 +0100

    Add form-input + button components to signin

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInSignUp from './pages/signIn-signup-page/signIn-signUp.component';
+
 import Header from './components/header/header.component';
 
 import { Route, Switch } from 'react-router-dom';
@@ -14,6 +16,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInSignUp} />
       </Switch>
     </div>
   );
---
+++ src/pages/signIn-signup-page/signIn-signUp.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import SignIn from '../../components/sign-in/sign-in.component';
+import './signIn-signup-page.styles.scss';
+
+const SignInSignUp = () => (
+    <div> sign In / up page
+        <SignIn/>
+    </div>
+)
+export default SignInSignUp;
+
