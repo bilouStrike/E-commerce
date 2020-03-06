255d7e3c7b89f5d4a84469a145e5e760a451f2c5
bilouStrike <bilal89dif@gmail.com>
2020-03-06 16:27:52 +0100

    add stripe system

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -4,6 +4,7 @@ import './checkout.styles.scss';
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
 import { connect } from 'react-redux';
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
 
 const CheckoutPage = ( {cartItems, total} ) => (
   <div className='checkout-page'>
@@ -29,6 +30,7 @@ const CheckoutPage = ( {cartItems, total} ) => (
        ))
      }
     <div className='total'>TOTAL: ${total}</div>
+    <StripeCheckoutButton price ={total} />
   </div>
 );
 
