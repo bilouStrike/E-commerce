81e0281dab356c84df00a0d2daac7ab1f2689157
bilouStrike <bilal89dif@gmail.com>
2020-03-15 12:25:31 +0100

    create server to manage stipte payment

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = 'pk_test_vZ2RCIUtfAgX8H9Wr1OhNE2l00ZnrpRnFW';
 
   const onToken = token => {
-    console.log(token);
-    alert('Payment Succesful!');
+      axios({
+          url: 'payment',
+          method: 'post',
+          data: {
+            amount: priceForStripe,
+            token
+          }
+      })
+        .then( response => {
+          alert('Payment success');
+        })
+        .catch( error => {
+          console.log(error);
+          alert('there is an issus');
+        })
   };
 
   return (
